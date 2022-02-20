a700bf168f82f483ff91816aa61995bcf5a1c2bc
Raznor <62889081+Raznor@users.noreply.github.com>
2022-02-20 18:06:06 +0100

    Added Get Next Character functionality

--- src/App.js
+++ src/App.js
@@ -2,48 +2,24 @@ import React from 'react';
 
 export default function App() {
   const [iceAndFireData, setIceAndFireData] = React.useState({});
-  const [count, setCount] = React.useState(0);
-  
-  // 1. GET the data (fetch)
-  // 2. Save the data to state
+  const [count, setCount] = React.useState(583);
+
   console.log("Component rendered");
   
-  /* this will create an infinite loop, because fetch calls setState, 
-  which causes the component to re-render and that in turn causes 
-  fetch to run again */
-  // fetch("https://sam-cors.herokuapp.com/https://anapioficeandfire.com/api/characters/583")
-  //   .then(res => res.json())
-  //   .then(data => setIceAndFireData(data));
-
-  /* setting a side effect without setting the dependency parameter
-  would still result in an infinite loop, although the fetch would
-  be forced to happen after the render is completed */
-  // React.useEffect(function () {
-  //   fetch("https://sam-cors.herokuapp.com/https://anapioficeandfire.com/api/characters/583")
-  //     .then(res => res.json())
-  //     .then(data => setIceAndFireData(data));
-  // });
-
-  /* an empty dependency list parameter lets React know that it should
-  only run the effect after the first render (when the component is mounted)
-  and then never again, because there are no conditions to watch for */
   React.useEffect(function () {
-    fetch("https://sam-cors.herokuapp.com/https://anapioficeandfire.com/api/characters/583")
+    console.log("Effect ran");
+    fetch(`https://sam-cors.herokuapp.com/https://anapioficeandfire.com/api/characters/${count}`)
       .then(res => res.json())
       .then(data => setIceAndFireData(data));
-  }, []);
-
-  /* if we're modifying state with some user input as in this case, the
-  state can serve as a dependency for the side effect function */
-  // React.useEffect(function () {
-  //   console.log('Effect ran');
-  // }, [count]);
+  }, [count]);
 
   return (
     <div className="container">
       <pre>{JSON.stringify(iceAndFireData, null, 2)}</pre>
       <h2>The count is {count}</h2>
-      <button onClick={() => setCount(prevCount => prevCount + 1)}>Add</button>
+      <button
+        onClick={() => setCount(prevCount => prevCount + 1)}
+      >Get Next Character</button>
     </div>
   );
 };
