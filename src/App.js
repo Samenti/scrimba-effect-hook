54424d8c228b60e89a7cea2684da0f537ea47a0c
Raznor <62889081+Raznor@users.noreply.github.com>
2022-02-20 17:15:57 +0100

    Infinite loop achieved by using setState() within fetch()

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
 export default function App() {
+  const [iceAndFireData, setIceAndFireData] = React.useState({})
+  
+  // 1. GET the data (fetch)
+  // 2. Save the data to state
+  console.log("Component rendered")
+  
+  /* this will create an infinite loop, because fetch calls setState, 
+  which causes the component to re-render and that in turn causes 
+  fetch to run again */
+  // fetch("https://sam-cors.herokuapp.com/https://anapioficeandfire.com/api/characters/583")
+  //   .then(res => res.json())
+  //   .then(data => setIceAndFireData(data));
+
   return (
     <div className="container">
-      <h1>App component</h1>
+      <pre>{JSON.stringify(iceAndFireData, null, 2)}</pre>
     </div>
   );
 };
