1f4d7a877f3a575b7b77d20be91e72411e7486aa
Raznor <62889081+Raznor@users.noreply.github.com>
2022-02-20 17:22:50 +0100

    Putting the fetch() into a side effect (useEffect()) is not enough

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 export default function App() {
-  const [iceAndFireData, setIceAndFireData] = React.useState({})
+  const [iceAndFireData, setIceAndFireData] = React.useState({});
   
   // 1. GET the data (fetch)
   // 2. Save the data to state
-  console.log("Component rendered")
+  console.log("Component rendered");
   
   /* this will create an infinite loop, because fetch calls setState, 
   which causes the component to re-render and that in turn causes 
@@ -13,6 +13,15 @@ export default function App() {
   // fetch("https://sam-cors.herokuapp.com/https://anapioficeandfire.com/api/characters/583")
   //   .then(res => res.json())
   //   .then(data => setIceAndFireData(data));
+
+  /* setting a side effect without setting the dependency parameter
+  would still result in an infinite loop, although the fetch would
+  be forced to happen after the render is completed */
+  // React.useEffect(function () {
+  //   fetch("https://sam-cors.herokuapp.com/https://anapioficeandfire.com/api/characters/583")
+  //     .then(res => res.json())
+  //     .then(data => setIceAndFireData(data));
+  // });
 
   return (
     <div className="container">
